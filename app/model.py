915a676e49cf1c3319ded5e5ba886f6729a68c03
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-18 16:48:33 +0100

    Add a constructor for 1 entry in the table PDBFile

--- app/model.py
+++ app/model.py
@@ -18,3 +18,21 @@ class PDBFile(db.Model):
     pdb_id = db.Column(db.String(4), primary_key=True)
     seq = db.Column(db.Text)
     resolution = db.Column(db.Float)
+
+    def __init__(self, pdb_id, seq, resolution):
+        """
+        constructor of one pdb file : PDBFile
+
+        Arguments :
+        ------------
+        pdb_id : 4-character string
+            the 4-character unique identifier of every entry in the Protein Data Bank
+        seq : string
+            the corresponding sequence using 1-letter AA code
+        resolution : float
+            a measure of the quality of the data that has been collected on
+            the crystal containing the protein or nucleic acid
+        """
+        self.pdb_id = pdb_id
+        self.seq = seq
+        self.resolution = resolution
