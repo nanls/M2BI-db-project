12c4de877b20f512986603802f936ae0898a2075
NicolasBourassin <bourassinnicolas@gmail.com>
2017-03-02 21:44:55 +0100

    Add Chain and Annotation table

--- app/model.py
+++ app/model.py
@@ -57,19 +57,50 @@ class Chain(db.Model):
     
     def __init__(self, chain_id, pdb_id, start, stop):
         """
-        constructor of one Chain instance : Chain
+        constructor of one chain instance : Chain
         Arguments :
         ------------
         chain_id : integer
+        pdb_id : 4-character string
+            the 4-character unique identifier of every entry in the Protein Data Bank
+        start : integer 
+            index of the first chain residue
+        stop : integer
+            index of the last chain residue 
+            (we assume that start < stop in the PDB file)
+        """
+        self.chain_id = chain_id
+        self.pdb_id = pdb_id
+        self.start = start
+        self.stop = stop
+
+    
+class Annotation(db.Model):
+    """
+    Attributes :
+    -----------
     pdb_id : 4-character string
         the 4-character unique identifier of every entry in the Protein Data Bank
-    start : integer 
-        index of the first chain residue
-    stop : integer
-        index of the last chain residue 
-        (we assume that start < stop in the PDB file)
+    method : string
+        the method used to produce the annotation
+    result : string
+        the string of annotation
     """
-    self.chain_id = chain_id
-    self.pdb_id = pdb_id
-    self.start = start
-    self.stop = stop
+    pdb_id = db.Column(db.String(4), foreign_key=True)
+    method = db.Column(db.String, foreign_key=True)
+    result = db.COlumn(db.String)
+    def __init__(self, pdb_id, method, result):
+         """
+        constructor of one annotation instance : Annotation
+        Arguments :
+        ------------
+        pdb_id : 4-character string
+            the 4-character unique identifier of every entry in the Protein Data Bank
+        method : string
+            the method used to produce the annotation
+        result : string
+            the string of annotation
+        """
+        self.pdb_id = pdb_id
+        self.method = method
+        self.result = result
