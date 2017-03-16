c03fc736163cff6950deb3aa243839dc32404f5f
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-17 00:25:36 +0100

    Rm trailing spaces

--- app/fetch-pdbs-from-PICSES-list.py
+++ app/fetch-pdbs-from-PICSES-list.py
@@ -4,38 +4,38 @@ from argparse import ArgumentParser
 import os
 import os.path
 
-import Bio.PDB  
+import Bio.PDB
 
 
 
 def is_valid_file(parser, arg):
     """ Check if arg filename is readable
-    
+
     Arguments :
     -----------
     parser : instance of argparse.ArgumentParser
-    
-    arg : string 
+
+    arg : string
         filename
-    
-    Return : 
+
+    Return :
     --------
     an open file handler
     """
-    
+
     if os.path.isfile(arg) and os.access(arg, os.R_OK):
-        print("File {0} exists and is readable".format (arg) ) 
+        print("File {0} exists and is readable".format (arg) )
         return open(arg, 'r')  # return an open file handle
     else:
         parser.error("Either file is missing or is not readable")
-    
+
 
 
 parser = ArgumentParser(description="fetch PDBs files using a PICSES list")
 parser.add_argument("-l", 
     dest="filename", required=True,
-    help="""input file PDB list. The first line is a header. 
-    Then, for each line, the fourth characters are PDB ids.""", 
+    help="""input file PDB list. The first line is a header.
+    Then, for each line, the fourth characters are PDB ids.""",
     metavar="FILE",
     type=lambda x: is_valid_file(parser, x)
 )
@@ -45,10 +45,10 @@ args = parser.parse_args()
 
 pdbl = Bio.PDB.PDBList()
 
-with args.filename as f : 
+with args.filename as f :
     next(f)
-    for line in f : 
+    for line in f :
         pdb_id = line[:4]
-        print (pdb_id) 
+        print (pdb_id)
         pdbl.retrieve_pdb_file(pdb_id, pdir = 'data')
-        
+
