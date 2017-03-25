18b77107655216cd1ed27f1ab723c71febba2dc6
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-25 17:26:02 +0100

    !!! Amend 7851b4b

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import configure_uploads, UploadSet
+
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
-pdb = UploadSet('pdb', ('pdb') )
-configure_uploads(app, pdb)
+pdb_set = UploadSet('pdb', ('pdb') )
+configure_uploads(app, pdb_set)
 
 db = SQLAlchemy(app)
