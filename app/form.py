4edfd1694ae1ef410478f98cffe1bc705476f9c8
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-25 18:19:35 +0100

    Fix create radio button

--- app/form.py
+++ app/form.py
@@ -6,7 +6,7 @@ They have several fields defined, and a CSRF token hidden field that is created
 automatically.
 """
 from flask_wtf import FlaskForm
-from wtforms.fields import SubmitField
+from wtforms.fields import SubmitField, RadioField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 import app
