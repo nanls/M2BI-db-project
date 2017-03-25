27fb4edca2c11d6b921c73c18090ca44e4cc811f
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-25 17:34:48 +0100

    Upload route

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 import flask
 
 from app import app
+from form import UploadForm
+
 @app.route("/")
 def index():
     """
     Define the basic route / and its corresponding request handler
     """
     return flask.render_template('index.html')
+
+
+@app.route("/upload")
+def upload():
+    """
+    Define the upload route
+    """
+    form  = UploadForm()
+    return flask.render_template('upload.html', form = form)
