84e9290652f6e60f941b2bf997c40ce4cd36baec
CharlottePerin <perincharlotte7@gmail.com>
2017-04-09 15:48:55 +0200

    Update form.py
    Création des formulaires pour la partie "conception de la base d'interrogation"

--- app/form.py
+++ app/form.py
@@ -25,3 +25,34 @@ class UploadForm(FlaskForm):
     submit = SubmitField('Upload file', render_kw={"class": "btn btn-info btn-lg", "id": "submit-button"})
 
     angle_unit = RadioField('Angle units', choices=[('degree','degree (default)'),('radian','radian')])
+
+    
+class SearchByPDBidForm(FlaskForm):
+	"""
+	Form for search in database by a PDB ID.
+	"""
+	PDBid = TextAreaField(u'PDB ID or list of PDB IDs separated by carriage return')
+	submit = SubmitField('Search', render_kw={"class": "btn btn-info btn-lg", "id": "submit-button"})
+
+
+class SearchFilesForm(FlaskForm):
+	"""
+	Form for display PDB in database according to resolution and/or 
+	criteria.
+	"""
+	# Resolution?
+	resMin = DecimalField('Minimum resolution', places = 2, rounding = None)
+	resMax = DecimalField('Maximum resolution', places = 2, rounding = None)
+	# Size?
+	sizeMin = IntegerField('Minimum size')
+	sizeMax = IntegerField('Maximum size')
+	submit = SubmitField('Display list of PDB id in database', render_kw={"class": "btn btn-info btn-lg", "id": "submit-button"})
+
+
+class SearchByKeyWD(FlaskForm):
+	"""
+	Form for search by keyword in the database.
+	"""
+	keywd = TextAreaField(u'Ex: integrin, Homo sapiens...')
+	submit = SubmitField('Search', render_kw={"class": "btn btn-info btn-lg", "id": "submit-button"})
+
