14af3ccdbd2079d766efc3d33a3881c544e2b162
NicolasBourassin <bourassinnicolas@gmail.com>
2017-04-09 19:18:01 +0200

    Corrections asked by Cha

--- app/views.py
+++ app/views.py
@@ -24,42 +24,40 @@ def upload():
         filename = pdb_set.save(
             storage = form.pdb_file.data, # The uploaded file to save
         )
-        print (filename) 
-        path = pdb_set.path(filename)
-        print (path)
-        dssp = annot.dsspAnnot(path)
-        pross = annot.prossAnnot(path)
-        angles = ramachandran.compute_phi_psi_angles(path, form.angle_unit.data)
-        print(angles)
-        ramachandran.compute_ramachandran_map(angles, form.angle_unit.data)
+        path = pdb_set.path(filename) #TEMP
+        print (path) #TEMP
+        angles = ramachandran.compute_phi_psi_angles(path, form.angle_unit.data) #TEMP
+        print(angles) #TEMP
+        ramachandran.compute_ramachandran_map(angles, form.angle_unit.data) #TEMP
         
-        
-        def insert(filename, dssp, pross):
-            """
-            Insertion of computed data into database
-            Arguments :
-            -----------
-            filename : string
-                name of the pdb file
-            dssp : string
-                annotation by dssp method
-            pross : string
-                annotation by pross method
-            
-            Return :
-            --------
-            None
-            """
-            
-            dssp_data = model.Annotation(pdb_id=filename[-8:-4], method="dssp", result=dssp)
-            #filename = "path/3xal.pdb", filename[-8:-4] = "3xal"
-            pross_data = model.Annotation(pdb_id=filename[-8:-4], method="pross", result=pross)
-            
-            #Add all annotations into db
-            db.session.add(dssp_data)
-            db.session.add(pross_data)
-            db.session.commit()
-            return
+        insert(filename)
         
         return "success"
     return flask.render_template('upload.html', form = form)
+
+
+def insert(filename):
+    """
+    Insertion of computed data into database
+    Arguments :
+    -----------
+    filename : string
+        name of the pdb file
+    dssp : string
+        annotation by dssp method
+    pross : string
+        annotation by pross method
+    Return :
+    --------
+    None
+    """
+    #TODO !!!!!!!!!!! add angles PHI & PSI
+    dssp_data = model.Annotation(pdb_id=filename[-8:-4], method="dssp", result=annot.dsspAnnot(path))
+    #filename = "path/3xal.pdb", filename[-8:-4] = "3xal"
+    pross_data = model.Annotation(pdb_id=filename[-8:-4], method="pross", result=annot.prossAnnot(path))
+
+    #Add all annotations into db
+    db.session.add(dssp_data)
+    db.session.add(pross_data)
+    db.session.commit()
+    return
