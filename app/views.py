18e5857c45e339f6e600c5ed69e1b565718679e5
CharlottePerin <perincharlotte7@gmail.com>
2017-04-11 22:44:05 +0200

    Update views.py
    Add the view for the detailed results page and the function that creates the string containing the aligned positions in sequence

--- app/views.py
+++ app/views.py
@@ -34,3 +34,45 @@ def upload():
         
         return "success"
     return flask.render_template('upload.html', form = form)
+
+
+def positionsPrinter(length):
+    """
+    Create a string containing the position numbers in a way that they will be
+    aligned with the sequence.
+    ARGUMENT:
+        length of the sequence to consider.
+    """
+    numbers = range(0, 10000, 10)
+    numbers[0] = 1
+    pos = ""
+    for i in xrange((length/10)+1):
+        if i == 0:
+            pos += "{:<9d}".format(numbers[0])
+        else:
+            pos += "{:<10d}".format(numbers[i])
+    return pos + "\n"
+
+
+@app.route("/results/<PDBid>")
+def resultsForOnePDB(PDBid, unit):
+    """
+    Define the detailed results route
+    """
+    pdb = model.PDBFile.query.get(PDBid)
+    #boundaries = model.Chain.query.get(PDBid)
+    sequence = pdb.seq#[boundaries.start:boundaries]
+    pos = positionsPrinter(len(sequence))
+    # Get the phi and psi angles and compute the Ramachandran map
+    angles = model.Angle.query.get(PDBid)
+    ramachandran.compute_ramachandran_map((angles.phi, angles.psi), unit)
+    # Get the annotations and stores them in a dictionary
+    annot = {}
+    annotations = model.Annotation.select(model.Annotation.pdb_id= \
+        PDBid).execute().all()
+    for meth in annotations:
+        annot["{<:7s}".format(annotations["method"])] = annotations["result"]
+    return flask.render_template('resultsForOnePDB.html', \
+        ramap = "temp/"+PDBid+".png", PDBid = PDBid, \
+        PDBsum="http://www.rcsb.org/pdb/explore/explore.do?structureId="+PDBid, \
+        positions = pos, sequence = sequence, annotations=annot)
