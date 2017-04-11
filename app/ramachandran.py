2979c67ef4781cbdbc53204271eb7dcb544fef3b
jecarvaill <carvaillojeancharles@gmail.com>
2017-04-11 21:55:19 +0200

    Revert "name in function of pdbid"
    This reverts commit 52175d94f2c4087c8f486762f7632912b92edb7b.

--- app/ramachandran.py
+++ app/ramachandran.py
@@ -142,10 +142,7 @@ def compute_ramachandran_map(angles, unit="degree"):
     # Changes figure size
     fig.set_size_inches(10.0, 10.0)
     # Saves figures
-    if not os.path.exists('tmp'):
-        os.mkdir('tmp')
-    name = os.path.basename(pdb_file)[0:4]
-    fig.savefig('tmp/' + name + '.png', dpi=300)
+    fig.savefig('temp/rama.png', dpi=300)
 
   
 if __name__ == "__main__":
