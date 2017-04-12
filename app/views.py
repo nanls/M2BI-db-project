5cd95508bcada336f15e4d881ea831643ceca4e6
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-04-12 02:18:32 +0200

    Ramap computation function will return the path of the ramap

--- app/views.py
+++ app/views.py
@@ -68,14 +68,14 @@ def resultsForOnePDB(PDBid, unit):
     # unzip list of tuple of (phi ,psi) for each angle
     print ( angles_phi, angles_psi )
 
-    ramachandran.compute_ramachandran_map((angles_phi, angles_psi), unit)
+    path = ramachandran.compute_ramachandran_map((angles_phi, angles_psi), unit)
     # Get the annotations and stores them in a dictionary
     annot = {}
     annotations = pdb.annotations
     for meth in annotations:
         annot["{<:7s}".format(annotations["method"])] = annotations["result"]
     return flask.render_template('resultsForOnePDB.html', \
-        ramap = "temp/"+PDBid+".png", PDBid = PDBid, \
+        ramap = path, PDBid = PDBid, \
         PDBsum="http://www.rcsb.org/pdb/explore/explore.do?structureId="+PDBid, \
         positions = pos, sequence = sequence, annotations=annot)
 
