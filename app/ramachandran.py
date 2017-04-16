969ae297250f2cabf2331bdf71a748c7964f788b
jecarvaill <carvaillojeancharles@gmail.com>
2017-04-16 22:01:29 +0200

    ramachandran map function adapted to all annotations methods

--- app/ramachandran.py
+++ app/ramachandran.py
@@ -41,7 +41,6 @@ from app import db
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-import mdtraj as md
 import model
 import os
 from sqlalchemy import or_, and_, func
@@ -52,35 +51,7 @@ import sys
 ######################################
 
 
-def compute_phi_psi_angles(pdb_file):
-    """Method to compute phi and psi angles of a pdb file in an given
-        unit (radian).
-
-    Arguments:
-        pdb_file: path of the pdb file
-
-    Returns:
-        phi_angles (numpy.ndarray): array which contains all phi angles
-        (numpy.float32).
-        psi_angles (numpy.ndarray): array which contains all psi angles
-        (numpy.float32).
-        phi_angles and psi angles are return in a tuple.
-
-    """
-    pdb = md.load_pdb(pdb_file)
-    phi_angles = md.compute_phi(pdb)
-    psi_angles = md.compute_psi(pdb)
-
-    # conserve radian degree
-    phi_angles = phi_angles[::][1][0]
-    psi_angles = psi_angles[::][1][0]
-
-    # right phi angle with right psi_angles
-    # phi angle position n and psi angle position n+1
-    return phi_angles[0:-1], psi_angles[1:]
-
-
-def compute_ramachandran_map(pdb_id, method, unit="radian"):
+def compute_ramachandran_map(pdb_id, unit="radian"):
     """Method to generate a ramachandran map with a given unit scale
         (radian or degree).
 
@@ -92,71 +63,73 @@ def compute_ramachandran_map(pdb_id, method, unit="radian"):
             path of Ramachandran map computed
 
     """
-    if method not in ['dssp', 'pross']:
-        sys.exit("Method of annotation specifeid doesn't exist.\n")
-    # get annotation
-    annotation = db.session.query(model.Annotation.result).filter(and_(model.Annotation.pdb_id==pdb_id,
-                                                                       model.Annotation.method==method))
-    print(annotation.scalar())
-    # get angles
-    angles = db.session.query(model.Angle).filter(model.Angle.pdb_id==pdb_id)
-    print(len(angles.all()))
-    x = [angles[i].phi for i in range(1,len(angles.all())-2)]
-    y = [angles[i].psi for i in range(1,len(angles.all())-2)]
-    if unit == "degree":
-        # conserve and convert radian angle to degree
-        x = np.rad2deg(x)
-        y = np.rad2deg(y)
-        x_label_in = "Phi(deg)"
-        y_label_in = "Psi(deg)"
-        plt.plot(x, y, ".")
-        # Sets x axis limits
-        plt.xlim(-180, 180)
-        # Sets y axis limits
-        plt.ylim(-180, 180)
-        # Sets ticks markers for x axis
-        plt.xticks(np.arange(-180.1, 180.1, 30))
-        # Sets ticks makers for y axis
-        plt.yticks(np.arange(-180.1, 180.1, 30))
-        # Adds x axis label
-        plt.xlabel(x_label_in)
-        # Adds y axis label
-        plt.ylabel(y_label_in)
-        plt.arrow(-180, 0, 360, 0)
-        plt.arrow(0, -180, 0, 360)
+    path = []
 
-    elif unit == "radian":
-        x_label_in = "Phi(rad)"
-        y_label_in = "Psi(rad)"
-        plt.plot(x, y, ".")
-        # Sets x axis limits
-        plt.xlim(-3.14, 3.14)
-        # Sets y axis limits
-        plt.ylim(-3.14, 3.14)
-        # Sets ticks markers for x axis
-        plt.xticks(np.arange(-3, 3.5, 0.5))
-        # Sets ticks makers for y axis
-        plt.yticks(np.arange(-3, 3.5, 0.5))
-        # Adds x axis label
-        plt.xlabel(x_label_in)
-        # Adds y axis label
-        plt.ylabel(y_label_in)
-        plt.arrow(-np.pi, 0, 2*np.pi, 0)
-        plt.arrow(0, -np.pi, 0, 2*np.pi)
+    for method in ['dssp', 'pross']:
+        # get annotation
+        annotation = db.session.query(model.Annotation.result).filter(and_(model.Annotation.pdb_id==pdb_id,
+                                                                           model.Annotation.method==method))
+        print(annotation.scalar())
+        # get angles
+        angles = db.session.query(model.Angle).filter(model.Angle.pdb_id==pdb_id)
+        print(len(angles.all()))
+        x = [angles[i].phi for i in range(1,len(angles.all())-2)]
+        y = [angles[i].psi for i in range(1,len(angles.all())-2)]
+        if unit == "degree":
+            # conserve and convert radian angle to degree
+            x = np.rad2deg(x)
+            y = np.rad2deg(y)
+            x_label_in = "Phi(deg)"
+            y_label_in = "Psi(deg)"
+            plt.plot(x, y, ".")
+            # Sets x axis limits
+            plt.xlim(-180, 180)
+            # Sets y axis limits
+            plt.ylim(-180, 180)
+            # Sets ticks markers for x axis
+            plt.xticks(np.arange(-180.1, 180.1, 30))
+            # Sets ticks makers for y axis
+            plt.yticks(np.arange(-180.1, 180.1, 30))
+            # Adds x axis label
+            plt.xlabel(x_label_in)
+            # Adds y axis label
+            plt.ylabel(y_label_in)
+            plt.arrow(-180, 0, 360, 0)
+            plt.arrow(0, -180, 0, 360)
 
-    # Create a file for plot
-    # Creates a figure
-    fig = plt.gcf()
-    # Changes figure size
-    fig.set_size_inches(10.0, 10.0)
-    # Saves figures
+        elif unit == "radian":
+            x_label_in = "Phi(rad)"
+            y_label_in = "Psi(rad)"
+            plt.plot(x, y, ".")
+            # Sets x axis limits
+            plt.xlim(-3.14, 3.14)
+            # Sets y axis limits
+            plt.ylim(-3.14, 3.14)
+            # Sets ticks markers for x axis
+            plt.xticks(np.arange(-3, 3.5, 0.5))
+            # Sets ticks makers for y axis
+            plt.yticks(np.arange(-3, 3.5, 0.5))
+            # Adds x axis label
+            plt.xlabel(x_label_in)
+            # Adds y axis label
+            plt.ylabel(y_label_in)
+            plt.arrow(-np.pi, 0, 2*np.pi, 0)
+            plt.arrow(0, -np.pi, 0, 2*np.pi)
 
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
+        # Create a file for plot
+        # Creates a figure
+        fig = plt.gcf()
+        # Changes figure size
+        fig.set_size_inches(10.0, 10.0)
+        # Saves figures
 
-    fig.savefig('temp/' + pdb_id + '_' + method + '.png', dpi=300)
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
 
-    return 'temp/' + pdb_id + '_' + method + '.png'
+        fig.savefig('temp/' + pdb_id + '_' + method + '.png', dpi=300)
+        path.append('temp/' + pdb_id + '_' + method + '.png')
+
+    return path
 
   
 if __name__ == "__main__":
@@ -169,12 +142,10 @@ if __name__ == "__main__":
     else:
         angle_unit = "radian"
     try:
-        angles = compute_phi_psi_angles(pdb_file)
-        print(angles)
         pdb_id = pdb_file[-8:-4]
         print(pdb_id)
     except:
         sys.exit("the specified file is not openable or not" +
                  " a pdb file or just not specified\n")
-    path_map = compute_ramachandran_map(pdb_id, "pross", angle_unit)
+    path_map = compute_ramachandran_map(pdb_id, angle_unit)
     print(path_map)
