ff45d77f6cfa4596b2d8a83815e32f3b8bdff25c
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-04-17 02:19:16 +0200

    Newline and spacing

--- app/config.py
+++ app/config.py
@@ -1,13 +1,14 @@
 """This module define classes with several available configuration for the app."""
 
+
 class Config(object):
 
-    #-----
+    # -----
     # general configuration :
     DEBUG = False
     TESTING = False
 
-    #-----
+    # -----
     # configuration of Flask-Uploads
     TOP_LEVEL_DIR = '.'
     UPLOADED_PDB_DEST = TOP_LEVEL_DIR + '/up'
@@ -16,12 +17,16 @@ class Config(object):
     # which db :
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
 
+
 class ProductionConfig(Config):
-    pass #empty because this database will not be really in production on a server
+    # empty because this database will not be really in production on a server
+    pass
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = 'dev'
+
 
 class TestingConfig(Config):
     TESTING = True
