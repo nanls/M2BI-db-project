84043efbb7894915a4dc932cce793c76d40f430d
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-04-17 02:16:45 +0200

    Trailing spaces

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config(object):
     # configuration of Flask-Uploads
     TOP_LEVEL_DIR = '.'
     UPLOADED_PDB_DEST = TOP_LEVEL_DIR + '/up'
-    
+
     # configuration of Flask-SQLAlchemy
     # which db :
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
